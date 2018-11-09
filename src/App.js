bbc4fb7663eaa959bdc9951cd5b815b14fdea428
derrick-long <long.derrick8@gmail.com>
2018-11-08 21:36:06 -0500

    add header function component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import Contact from './components/Contact';
-
+import Header from './components/Header';
 import './App.css';
 // execute jsx before the return 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <h1> The App Component </h1> 
-       <Contact />
+        <Header />
+        <Contact />
       </div>
     );
   }
