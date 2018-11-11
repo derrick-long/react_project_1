237966aab0882eca8fa91df8c8becaaee682976f
derrick-long <long.derrick8@gmail.com>
2018-11-10 22:31:58 -0500

    add and implement bootstrap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React, { Component } from 'react';
 import Contact from './components/Contact';
 import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './App.css';
 // execute jsx before the return 
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         
-        <Header branding="Contact Manger"/>
+        <Header branding= "Contact Manager" />
         
-        <Contact name="John Doe" 
-        email="jdoe@gmail.com" phone="555-555-5555"
-        />
-      
-        <Contact name = "Karen Smith"
-        email = "ksmith@gmail.com"
-        phone = "444-444-4444" 
-        />
-       </div>
+        <div className="container">
+
+          <Contact name="John Doe" 
+          email="jdoe@gmail.com" phone="555-555-5555"
+          />
+        
+          <Contact name = "Karen Smith"
+          email = "ksmith@gmail.com"
+          phone = "444-444-4444" 
+          />
+        </div>
+        
+    </div>
     );
   }
 }
