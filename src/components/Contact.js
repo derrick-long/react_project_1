7ac770af4889353498be4fd3943096ce083aec19
derrick-long <long.derrick8@gmail.com>
2018-11-11 22:51:11 -0500

    add state to contacts

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,16 +2,27 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
  class Contact extends Component {
+    state = {
+        showContactInfo: false
+    };
+
+
   render() {
       const { contact } = this.props;
+      const { showContactInfo } = this.state;
+    
     return (
       <div className="card card-body mb-3">
-        <h4>{contact.name}</h4> 
-        <ul className="list-group">
-            <li className="list-group-item">Email: {contact.email}</li>
-            <li className = "list-group-item"> Phone: 
-            {contact.phone} </li>
+        <h4>{contact.name} <i onClick={() => 
+        this.setState({showContactInfo: !this.state.showContactInfo})
+        }
+        className="fas fa-sort-down"> </i></h4 >
+        {showContactInfo ? ( 
+        <ul className = "list-group">
+            <li className = "list-group-item" > Email: {contact.email} </li> 
+            <li className = "list-group-item" > Phone: {contact.phone} </li> 
         </ul>
+        ) : null}
       </div>
     )
   }
