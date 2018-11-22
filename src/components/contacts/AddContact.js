a83bd4198418719d84b035f370340864d51ce2d2
derrick-long <long.derrick8@gmail.com>
2018-11-21 19:59:15 -0500

    add async await

--- src/components/contacts/AddContact.js
+++ src/components/contacts/AddContact.js
@@ -14,7 +14,7 @@ import axios from 'axios';
 
     onChange = e => this.setState({ [e.target.name]: e.target.value });
 
-    onSubmit = (dispatch,e) => {
+    onSubmit  = async (dispatch,e) => {
        e.preventDefault();
 
        const { name, email, phone }= this.state;
@@ -45,9 +45,11 @@ import axios from 'axios';
            phone
        };
 
-       axios.post('https://jsonplaceholder.typicode.com/users', newContact)
-       .then(res => 
-        dispatch({type: 'ADD_CONTACT', payload: res.data}))
+       const res =  await axios
+        .post('https://jsonplaceholder.typicode.com/users', newContact);
+
+        dispatch({type: 'ADD_CONTACT', payload: res.data});
+    
 
 
        //clear state
--- src/components/contacts/Contact.js
+++ src/components/contacts/Contact.js
@@ -8,13 +8,15 @@ import axios from 'axios';
         showContactInfo: false
     };
 
-    onDeleteClick = (id, dispatch) => {
-        axios.delete
-        (`https://jsonplaceholder.typicode.com/users/${id}`)
-        .then(res => dispatch({ type: 'DELETE_CONTACT', payload: id}))
-
-    };
-
+    onDeleteClick = async (id, dispatch) => {
+        try{
+            await axios.delete
+            (`https://jsonplaceholder.typicode.com/users/${id}`);
+            dispatch({ type: 'DELETE_CONTACT', payload: id});
+        } catch(e){
+            dispatch({ type: 'DELETE_CONTACT', payload: id});
+        }
+    }
 
   render() {
       const { id, name, email, phone} = 
