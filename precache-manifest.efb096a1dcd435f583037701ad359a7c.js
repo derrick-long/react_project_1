07d7b24961befea9f9753ff642019b60148de6af
derrick-long <long.derrick8@gmail.com>
2018-11-26 22:37:06 -0500

    Updates

---
+++ precache-manifest.efb096a1dcd435f583037701ad359a7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c93975bf0255cc14b10f",
+    "url": "/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "c93975bf0255cc14b10f",
+    "url": "/static/js/main.c93975bf.chunk.js"
+  },
+  {
+    "revision": "a44558aaa4bbf797caf1",
+    "url": "/static/css/1.978bb234.chunk.css"
+  },
+  {
+    "revision": "a44558aaa4bbf797caf1",
+    "url": "/static/js/1.a44558aa.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "91d0191e4596f016a63ec0ef4f121c5b",
+    "url": "/index.html"
+  }
+];
